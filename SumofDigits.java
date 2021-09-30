f47dd8d7802e8b90b0e327cc5c8785ef7ccd163a
KumarRajnish1997 <91653177+KumarRajnish1997@users.noreply.github.com>
2021-09-30 11:38:34 +0530

    Add files via upload

---
+++ SumofDigits.java
@@ -0,0 +1,26 @@
+package com.broadridge.assignment2;
+
+public class SumofDigits {
+
+	static int findSum(int number)  
+	{         
+	int sum = 0;   
+	while (number != 0)  
+	{      
+	sum = sum + number % 10;    
+	number = number/10;  
+	}  
+	//returns the summm 
+	return sum;  
+	}  
+	//driver code  
+	public static void main(String args[])  
+	{  
+	int number = 1234;  
+	//calling the user-defined method and prints the result   
+	System.out.println("The sum of digits: "+findSum(number));  
+	}  
+	
+
+}
+
